5d1ce34166bfd62150bbb5f8b31c5fd493444aac
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-07-10 20:55:17 +0600

    chore(utils): centralize prisma export and widen utils barrel

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,5 @@ export { default as catchAsync } from "./catchAsync";
 export { default as ApiError } from "./apiError";
 export { hashPassword, comparePassword } from "./hashPassword";
 export { getPagination } from "./pagination";
+export { default as apiError } from "./apiError";
+export { default as prisma } from "../config/db";
